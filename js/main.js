58b2275ef3daced79df6a1c83408cec9da3eab08
Dmitrii Gachkovskii <h1ckzvk@gmail.com>
2023-03-01 17:11:28 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -11,11 +11,7 @@ $(function(){
     let clearForm = function(){
         $('.popup-container > input[type="text"]').val('');
     };
-
-    $('.order-call').on('click', function(){
-        showPopup();
-    });
-
+    
     $('.close').on('click', function(){
         clearForm();
         closePopup();
